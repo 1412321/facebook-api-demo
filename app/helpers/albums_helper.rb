c42600fbcf424ec274e4dd1b01b5dd6bbd55421f
minhle <minhle@teqnological.asia>
2017-10-08 02:51:21 +0700

    Add logic to layout

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -1,2 +1,15 @@
 module AlbumsHelper
+  def strtodate(str)
+    date = Date.parse str
+  end
+
+  def content_preview(content)
+    if content
+      if(content.length > 100)
+        content[0..99] + " ..."
+      else
+        content
+      end
+    end
+  end
 end
